af00ef13157dc64d337f931ff9ea4d9402e64e99
roman-kart <romankartasheff@gmail.com>
2022-03-02 21:19:43 +0300

    interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using Interfaces;
+
+TestInterface TestInterface = new TestInterface();
+Console.WriteLine("TestInterface: ");
+TestInterface.VirtualSealed();
+TestInterface.VirtualNonSealed();
+Console.WriteLine();
+
+Console.WriteLine("ITestVirtualMethods: ");
+ITestVirtualMethods testVirtualMethods = new TestInterface();
+testVirtualMethods.VirtualSealed();
+testVirtualMethods.VirtualNonSealed();
---
+++ Interfaces/TestInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ITestVirtualMethods
+    {
+        public void VirtualSealed();
+        public virtual void VirtualNonSealed()
+        {
+            Console.WriteLine("ITestVirtualMethods.VirtualNonSealed");
+        }
+    }
+    internal class TestInterface : ITestVirtualMethods
+    {
+        public void VirtualSealed()
+        {
+            Console.WriteLine("TestInterface.VirtualSealed");
+        }
+        public void VirtualNonSealed()
+        {
+            Console.WriteLine("TestInterface.VirtualNonSealed");
+        }
+    }
+}
