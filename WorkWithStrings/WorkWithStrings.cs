25ea35015daeab9e782f9a4b20e25a0dd3df9a43
roman-kart <romankartasheff@gmail.com>
2022-03-03 10:43:06 +0300

    StringComparisonEnd

--- WorkWithStrings/WorkWithStrings.cs
+++ WorkWithStrings/WorkWithStrings.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Globalization;
 
 namespace WorkWithStrings
 {
@@ -25,10 +27,47 @@ namespace WorkWithStrings
 
             ShowCompareResult(str1, "ELLO!", str1.EndsWith("ELLO!", StringComparison.CurrentCultureIgnoreCase), "end with", "ingnoring case, current culture");
             ShowCompareResult(str1, "ELLO!", str1.EndsWith("ELLO!"), "end with", "not ingnoring case");
+
+            var cultures = System.Globalization.CultureInfo.GetCultures(System.Globalization.CultureTypes.NeutralCultures);
+            foreach (var culture in cultures)
+            {
+                Console.WriteLine($"{culture}: {culture.NativeName}");
+            }
+
+            // CurrentCulture
+            ShowCurrentThreadCultures();
+
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US", false);
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US", false);
+            ShowCurrentThreadCultures();
+
+            ChangeCurrentCulture("ar-SA");
+            ShowCurrentThreadCultures();
+
+            ChangeCurrentCulture("ka-GE");
+            ShowCurrentThreadCultures();
+
+            ChangeCurrentCulture("ml-IN");
+            ShowCurrentThreadCultures();
+
+            var ci = new CultureInfo("en-US");
+            var comi = ci.CompareInfo;
         }
         private static void ShowCompareResult<TVal, TResult>(TVal val1, TVal val2, TResult result, string operation, string addition)
         {
             Console.WriteLine($"\"{val1}\" {operation} \"{val2}\" ({addition})? Result: {result}");
+        }
+        private static void ShowCurrentThreadCultures()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Thread.CurrentThread.CurrentCulture: {Thread.CurrentThread.CurrentCulture}");
+            Console.WriteLine($"Thread.CurrentThread.CurrentUICulture: {Thread.CurrentThread.CurrentUICulture}");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine();
+        }
+        private static void ChangeCurrentCulture(string code)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(code, false);
         }
     }
 }
