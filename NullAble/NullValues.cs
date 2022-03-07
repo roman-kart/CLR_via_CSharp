a007eae6937ce2ee5097d15090ef1902197cadca
roman-kart <romankartasheff@gmail.com>
2022-03-07 13:01:07 +0300

    NullAbleComplete

---
+++ NullAble/NullValues.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace NullAble
+{
+    internal class NullValues
+    {
+        public static int? GetNullAble()
+        {
+            int? result = RandomNumberGenerator.GetInt32(-10, 30);
+            result = result % 2 == 0 ? null : result;
+            return result;
+        }
+        public static void ChangeIntObj(ref Object obj)
+        {
+            obj = 102342142;
+        }
+        public static void ChangeIntObj(Object obj)
+        {
+            obj = 102342142;
+        }
+    }
+}
--- NullAble/Program.cs
+++ NullAble/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿Nullable<Int32> nullInt = null;
+Nullable<Int32> notNullInt = 10;
+
+Console.WriteLine($"nullInt = {nullInt.HasValue}");
+Console.WriteLine($"notNullInt = {notNullInt}");
+
+// with null
+nullInt++;
+nullInt--;
+nullInt += nullInt;
+nullInt -= 2;
+nullInt *= nullInt;
+nullInt /= 5;
+Console.WriteLine($"nullInt = {nullInt.HasValue}");
+
+notNullInt++;
+notNullInt--;
+notNullInt *= notNullInt;
+notNullInt /= 5;
+notNullInt += notNullInt;
+notNullInt -= 3;
+Console.WriteLine($"notNullInt = {notNullInt}");
+
+// оператор объединения null-совместимых типов
+int i = NullValues.GetNullAble() ?? int.MinValue;
+Console.WriteLine(i);
+
+i = NullValues.GetNullAble() ?? NullValues.GetNullAble() ?? NullValues.GetNullAble() ?? int.MaxValue; // несколько сравнений
+Console.WriteLine(i);
+
+int? a = null;
+Object obj = a;
+NullValues.ChangeIntObj(a);
+Console.WriteLine(a);
+
+a = 5;
+obj = a;
+Console.WriteLine((Int32?)obj);
+Console.WriteLine((Int32)obj);
+
+a = null;
+obj = a;
+Console.WriteLine((Int32?)obj);
+// Console.WriteLine((Int32)obj); // NullReferenceException
+
+int? aa;
+aa = null;
+// Console.WriteLine(aa.GetType()); // NullReferenceException
+aa = 5;
+Console.WriteLine(aa.GetType()); // Int32
+
+int? aaa = 5;
+Console.WriteLine(((IComparable)aaa).CompareTo(5));
+aaa = null;
+// Console.WriteLine(((IComparable)aaa).CompareTo(5)); // NullReferenceException
